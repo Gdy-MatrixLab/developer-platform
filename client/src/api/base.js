ffc3dc15e9730d56ac00b9044a966137c41d5691
王含泽 <whzcorcd@gmail.com>
2020-07-28 18:06:13 +0800

    fix 1. fix produciton environment request urls

--- client/src/api/base.js
+++ client/src/api/base.js
@@ -2,14 +2,14 @@
  * @Author: Whzcorcd
  * @Date: 2020-07-16 17:32:03
  * @LastEditors: Wzhcorcd
- * @LastEditTime: 2020-07-20 20:01:11
+ * @LastEditTime: 2020-07-28 18:01:18
  * @Description: file content
  */
 const base = {
   gateWay:
     process.env.NODE_ENV === 'development'
       ? '//127.0.0.1:7001/api'
-      : '//dc.guangdianyun.tv'
+      : '//devops.guangdianyun.tv/api'
 }
 
 export default base
